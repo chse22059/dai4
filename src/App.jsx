7df0ba79e0ef6ea52f9deff6eb98bf646abe7a16
[chse] <chse22059@g.nihon-u.ac.jp>
2023-10-22 21:26:42 +0900

    dai

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-
 export default function App() {
     return (
       <div>
+        <img src="public/dove.png" />
+        <img src="images/favorite-1.jpg" />
+        <img src="images/favorite-2.jpg" />
+        <img src="images/favorite-3.jpg" />
+        <img src="images/favorite-4.jpg" />
         <Header />
         <Main />
         <Footer />
