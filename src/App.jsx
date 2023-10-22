07e932d71cd84a987a3db8637ede0f937e1ad44a
[chse] <chse22059@g.nihon-u.ac.jp>
2023-10-22 21:27:55 +0900

    dai

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ export default function App() {
     return (
       <div>
         <img src="public/dove.png" />
-        <img src="images/favorite-1.jpg" />
-        <img src="images/favorite-2.jpg" />
-        <img src="images/favorite-3.jpg" />
-        <img src="images/favorite-4.jpg" />
+        <img src="public/favorite-1.jpg" />
+        <img src="public/favorite-2.jpg" />
+        <img src="public/favorite-3.jpg" />
+        <img src="public/favorite-4.jpg" />
         <Header />
         <Main />
         <Footer />
