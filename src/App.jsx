c933fdb87b783e9af10239a3b775992a22faf0a1
[chse] <chse22059@g.nihon-u.ac.jp>
2023-10-22 22:46:35 +0900

    dai4

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,8 @@ import Footer from "./components/Footer";
 export default function App() {
     return (
       <div>
-        <Header>
-        <img src="public/dove.png" />
-        </Header>
-        <Main>
-        <img src="public/favorite-1.jpg" />
-        <img src="public/favorite-2.jpg" />
-        <img src="public/favorite-3.jpg" />
-        <img src="public/favorite-4.jpg" />
-        </Main>
+        <Header />
+        <Main />
         <Footer />
       </div>
     );
