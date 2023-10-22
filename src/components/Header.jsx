d663403058e5c173ada975b83f84decc851f2e40
[chse] <chse22059@g.nihon-u.ac.jp>
2023-10-22 20:16:31 +0900

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Birdwatching</h1>
+            <div className="header img">
+                <img src="dove.png" alt="a simple dove logo"></img>
+            </div>
+            <nav>
+                <ul>
+                    <li><span><strong>Home</strong></span></li>
+                    <li><a href="#"><strong>Get started</strong></a></li>
+                    <li><a href="#"><strong>Photos</strong></a></li>
+                    <li><a href="#"><strong>Gear</strong></a></li>
+                    <li><a href="#"><strong>Forum</strong></a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
